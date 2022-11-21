d6016322da6a2b272446c1ec91cec4cd0e085aa9
julianlombana <110684866+julianlombana@users.noreply.github.com>
2022-11-21 13:46:45 -0500

    hola

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+import java.util.Scanner;
+
+public class julian {
+    public julian(){
+
+
     }
-}
+
+    public static void main(String[] args) {
+        System.out.println("Hola empleado, por favor digite:");
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("su nombre");
+        String cadena = entrada.nextLine();
+        System.out.println("su apellido");
+        String cadena1 = entrada.nextLine();
+        System.out.println("su edad");
+        int num = entrada.nextInt();
+        System.out.println("¿cuantas horas trabaja?");
+        int num1 = entrada.nextInt();
+        System.out.println(" cual es tu nombre es " + cadena + " su apellido es " + cadena1);
+        System.out.println("su edad es " + num);
+        System.out.println("usted trabaja: " + num1 + " horas");
+        System.out.println("su salario es de " + 800000 * num1);
+    }
+
